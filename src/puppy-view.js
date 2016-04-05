56264e26d42faf9b688fb2779cd6219c8ac01260
Emily Znamierowski <emilyannz@gmail.com>
2016-04-05 14:47:53 -0500

    changed class naming conventions to better match assignment to stop confusing myself

--- src/puppy-view.js
+++ src/puppy-view.js
@@ -8,38 +8,38 @@ export default class PuppyView {}
     this.about = about;
     this.renderpuppy();
   }
-  renderpuppy() {
-    this.newform = document.createElement('div');
-    this.newform.classList.add('puppy-profile')
-    this.row = document.querySelector('.row');
-    this.row.appendChild(this.newform);
-    this.newform.innerHTML = `
-    <div class="puppy-profile__image">
-      <img src="${this.photoUrl}" alt="" class="puppy-form__image"/>
-    </div>
 
-    <div class="puppy-profile__form">
-      <ul class="puppy-profile__form-inputs">
-        <li class="puppy-profile__form-inputs-item">
+  renderpuppy() {
+    this.newcard = document.createElement('div');
+    this.newcard.classList.add('puppy-card')
+    this.row = document.querySelector('.row');
+    this.row.appendChild(this.newcard);
+    this.newcard.innerHTML = `
+    <div class="puppy-card__image">
+      <img src="${this.photoUrl}" alt="" class="puppy-card__pic"/>
+    </div>
+    <div class="puppy-card__form">
+      <ul class="puppy-card__form-inputs">
+        <li class="puppy-card__form-inputs-item">
           <h6 class="inputs-item__label">Name</h6>
-          <input type="text" class="puppy-profile__inputs-item__input" value="${this.name}">
+          <input type="text" class="puppy-card__inputs-item__input" value="${this.name}">
         </li>
-        <li class="puppy-profile__form-inputs-item">
+        <li class="puppy-card__form-inputs-item">
           <h6 class="inputs-item__label">Age</h6>
-          <input type="text" class="puppy-profile__inputs-item__input" value="${this.age}">
+          <input type="text" class="puppy-card__inputs-item__input" value="${this.age}">
         </li>
-        <li class="puppy-profile__form-inputs-item">
+        <li class="puppy-card__form-inputs-item">
           <h6 class="inputs-item__label">Photo URL</h6>
-          <input type="text" class="puppy-profile__inputs-item__input" value="${this.photoUrl}">
+          <input type="text" class="puppy-card__inputs-item__input" value="${this.photoUrl}">
         </li>
-        <li class="puppy-profile__form-inputs-item">
+        <li class="puppy-card__form-inputs-item">
           <h6 class="inputs-item__label">Profile</h6>
-          <input type="text" class="puppy-profile__inputs-item__input" value="${this.profile}">
+          <input type="text" class="puppy-card__inputs-item__input" value="${this.profile}">
         </li>
       </ul>
       <div class ="button-row">
-      <button class ="puppy-profile__button delete-button">Delete</button>
-      <button class ="puppy-profile__button update-button">Update</button>
+      <button class ="puppy-card__button delete-button">Delete</button>
+      <button class ="puppy-card__button update-button">Update</button>
       </div>
     </div>`;
       }
