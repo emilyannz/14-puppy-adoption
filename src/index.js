39ebdcf67ecb5e9d2949a1875cd72a66514fb51c
Emily Znamierowski <emilyannz@gmail.com>
2016-04-25 19:04:49 -0500

    Create index module

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 // Create `index` module that exports a function to kick things off
 'use strict';
+import AppView from 'app-view';
 
-import ApplicationView from 'application';
-
-export default function() {
-  const newPuppytest = new ApplicationView;
+export default function () {
+  const appView = new AppView(document.querySelector(`body`));
 }
